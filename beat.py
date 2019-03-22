65fdf8ddad5830b1343394386e017a5a2079686e
Dmitry Yakhnov <dmitry@yakhnov.info>
2019-03-22 14:18:31 +1100

    Beat health check to use UTC

--- beat.py
+++ beat.py
@@ -1,12 +1,9 @@
 from datetime import datetime, timedelta
 import shelve
 
-now = datetime.now(tz=None)
+now = datetime.utcnow()
 file_data = shelve.open('celerybeat-schedule') # Name of the file used by PersistentScheduler to store the last run times of periodic tasks.
 
 if 'entries' in file_data:
   for task_name, task in file_data['entries'].items():
-    try:
-      assert now  < task.last_run_at.replace(tzinfo=None) + task.schedule.run_every
-    except AttributeError:
-      assert timedelta() < task.schedule.remaining_estimate(task.last_run_at)
+    assert now < task.last_run_at.replace(tzinfo=None) + task.schedule.run_every
